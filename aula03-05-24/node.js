1a49f4590ea7f4b8ddc61fe5e99abbaf383c2aad
Rafaela Mayumi Wada Fukuda <125597771+RafaelaMayumiFukuda@users.noreply.github.com>
2024-05-03 18:24:13 -0300

    Add files via upload

---
+++ aula03-05-24/node.js
@@ -0,0 +1,15 @@
+function primeiro(id){
+    if (id == 1){
+        alert("Correto");
+    }else{
+        alert("Incorreto");
+    }
+}
+
+function segundo(x){
+    if (x % 2 == 0){
+        alert("Par");
+    }else{
+        alert("Ímpar");
+    }
+}
