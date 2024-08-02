9d9b1e122cbf5b63cb7ba8c060c709cfe583d2a6
Rafaela Mayumi Wada Fukuda <125597771+RafaelaMayumiFukuda@users.noreply.github.com>
2024-08-02 18:15:34 -0300

    Add files via upload

--- Aula05/code.js
+++ Aula05/code.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $( "#btnome" ).on( "click", function()  {
-        var nome = $("#inputNome").val();
+        var nome = $("#txtNome").val();
         alert("Nome selecionado: " + nome);
     })
     $( "#btidade" ).on( "click", function()  {
@@ -16,6 +16,14 @@ $(document).ready(function(){
                     alert("Por favor, selecione um gênero.");
                 }
     })
+    $( "#btestadocvl" ).on( "click", function()  {
+        var estadocvl = $("#cvl").val();
+                if (estadocvl) {
+                    alert("Estado selecionado: " + estadocvl);
+                } else {
+                    alert("Por favor, selecione um estado.");
+                }
+    })
     $( "#btrg" ).on( "click", function()  {
         var rg = $("#txtrg").val();
         alert("RG selecionado: " + rg);
@@ -24,15 +32,36 @@ $(document).ready(function(){
         var cpf = $("#txtcpf").val();
         alert("CPF selecionado: " + cpf);
     })
+    $( "#bttell" ).on( "click", function()  {
+        var tell = $("#txtTell").val();
+        alert("Telefone selecionado: " + tell);
+    })
+    $( "#btcell" ).on( "click", function()  {
+        var cell = $("#txtCell").val();
+        alert("CPF selecionado: " + cell);
+    })
+    $( "#btemail" ).on( "click", function()  {
+        var email = $("#txtEmail").val();
+        alert("E-mail selecionado: " + email);
+    })
+    $( "#btnasc" ).on( "click", function()  {
+        var nasc = $("#txtnasc").val();
+        alert("Data de Nascimento selecionado: " + nasc);
+    })
 
     $( "#btcadastro" ).on( "click", function()  {
-        var nome = $("#inputNome").val();
+        var nome = $("#txtNome").val();
         var idade = $("#txtidade").val();
         var genero = $("#cmb").val();
         var rg = $("#txtrg").val();
         var cpf = $("#txtcpf").val();
-        if (nome && idade && genero && rg && cpf) {
-            alert("Cadastrado com sucesso!\nNome: " + nome + "\nIdade: " + idade + "\nGênero: " + genero + "\nRG: " + rg + "\nCPF: " + cpf);
+        var estadocvl = $("#cvl").val();
+        var tell = $("#txtTell").val();
+        var cell = $("#txtCell").val();
+        var email = $("#txtEmail").val();
+        var nasc = $("#txtnasc").val();
+        if (nome && idade && genero && rg && cpf && estadocvl && tell && cell && email && nasc) {
+            alert("Cadastrado com sucesso!\nNome: " + nome + "\nIdade: " + idade + "\nNascimento: "+ nasc +"\nGênero: " + genero + "\nRG: " + rg + "\nCPF: " + cpf + "\nEstado Civil: " + estadocvl + "\nTelefone: " + tell + "\nCelular: " + cell + "\nEmail: " + email );
         } else {
            
             alert("Por favor, preencha todos os campos antes de cadastrar.");
