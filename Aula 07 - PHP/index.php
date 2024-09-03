8cf66a387d7d2a5d3bdaa5f57ffb9ff382082e77
Rafaela Mayumi Wada Fukuda <125597771+RafaelaMayumiFukuda@users.noreply.github.com>
2024-09-03 18:28:03 -0300

    Add files via upload

---
+++ Aula 07 - PHP/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $Nome = "flut";
+    echo "Nome: $Nome","<br>";
+
+?>
+
+<?php
+
+    $Lista = array(
+
+        array("Bolo", "Croissant", "Tiramisu", "Palha Italiana", "Cupcake"),
+        array(1, 2, 3, 4)
+
+    );
+
+    echo $Lista[0][2],"<br>";
+    echo $Lista[1][3]
+?>
+
